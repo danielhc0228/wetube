0a3a6db94ed5d2695f1c918b7fb601e3e669de91
Daniel Chung <daniel20020228@gmail.com>
2025-09-22 18:19:54 +1000

    add middleware

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import express from "express";
 
-const PORT = 4000;
+const PORT = 4001;
 
 const app = express();
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+const handleHome = (req, res) => {
+    return res.send("I love middlewares");
+};
+
+app.get("/", logger, handleHome);
 
 const handleListening = () =>
     console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
