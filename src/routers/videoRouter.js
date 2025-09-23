4415bf7dd85eadd739357b16e5ba1f1a3a4e0df8
Daniel Chung <daniel20020228@gmail.com>
2025-09-23 17:57:59 +1000

    add more routes

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { edit, watch } from "../controllers/videoController";
+import {
+    deleteVideo,
+    edit,
+    see,
+    upload,
+    watch,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
